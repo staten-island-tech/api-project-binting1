17ed146f3c6e084d514d15df49e7c3be7219ebaa
binting1 <bintingchenn123@gmail.com>
2024-11-18 14:02:29 -0500

    123

--- app/main.js
+++ app/main.js
@@ -2,20 +2,17 @@ import "./style.css";
 async function getData () {
     try {
        //returns a promise
-    const response = await fetch ('data.json'); 
+    const response = await fetch ('https://api.disneyapi.dev/character'); 
         //gaurd clause
     if(response.status != 200) {
         throw new Error(response);
     } else {
         //converts promise to json
     const data = await response.json();
-    console.log(data.data);
+    console.log(data.data)
     //this is unique to this API
-    data.data.forEach((agent) => console.log(agent.displayName));
-    // data.data.forEach(agent) => document
-    // .querySelector("div")
-    // .insertAdjacentElement("afterbegin," `<h1>${agent.displayName}</h1>`
-    // );
+    // data.data.forEach((agent) => console.log(agent.name));
+    data.data.forEach((element) => document.querySelector("div").insertAdjacentHTML("afterbegin,", `<h1>${element.name}</h1>`));
 }
 
     } catch (error) {
