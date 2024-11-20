526d6fc539938943ffbe6e77d0c4662bac883e2c
binting1 <bintingchenn123@gmail.com>
2024-11-20 14:03:33 -0500

    got it to work

--- app/main.js
+++ app/main.js
@@ -2,18 +2,17 @@ import "./style.css";
 async function getData () {
     try {
        //returns a promise
-    const response = await fetch ('https://gsi.fly.dev/characters'); 
+    const response = await fetch ('https://api.potterdb.com/v1/characters'); 
         //gaurd clause
-        // https://api.potterdb.com/v1/characters
     if(response.status != 200) {
         throw new Error(response);
     } else {
         //converts promise to json
     const data = await response.json();
-    console.log(data.results);
+    console.log(data.data)
     //this is unique to this API
     // data.data.forEach((agent) => console.log(agent.name));
-    data.results.forEach((result) => document.querySelector("div").insertAdjacentHTML("beforeend,", `<h1>${result.name}</h1>`));
+    data.data.forEach((smtg) => document.querySelector("div").insertAdjacentHTML("beforeend", `<h1>${smtg.attributes}</h1>`));
 }
 
     } catch (error) {
