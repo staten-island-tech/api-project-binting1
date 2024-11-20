91e0b249425fd9b187a274b6799fcc2d10f4ba2d
binting1 <bintingchenn123@gmail.com>
2024-11-20 13:36:42 -0500

    123

--- app/main.js
+++ app/main.js
@@ -2,17 +2,18 @@ import "./style.css";
 async function getData () {
     try {
        //returns a promise
-    const response = await fetch ('https://api.potterdb.com/v1/characters'); 
+    const response = await fetch ('https://gsi.fly.dev/characters'); 
         //gaurd clause
+        // https://api.potterdb.com/v1/characters
     if(response.status != 200) {
         throw new Error(response);
     } else {
         //converts promise to json
     const data = await response.json();
-    console.log(data.data)
+    console.log(data.results);
     //this is unique to this API
     // data.data.forEach((agent) => console.log(agent.name));
-    data.data.forEach((type) => document.querySelector("div").insertAdjacentHTML("afterbegin,", `<h1>${type.name}</h1>`));
+    data.results.forEach((result) => document.querySelector("div").insertAdjacentHTML("beforeend,", `<h1>${result.name}</h1>`));
 }
 
     } catch (error) {
