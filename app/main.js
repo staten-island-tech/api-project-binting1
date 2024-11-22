67f41074d1dbb2af9cdc389208fd5fc3e6cbecff
binting1 <bintingchenn123@gmail.com>
2024-11-22 11:02:23 -0500

    123

--- app/main.js
+++ app/main.js
@@ -2,26 +2,26 @@ import "./style.css";
 async function getData () {
     try {
        //returns a promise
-    const response = await fetch ('https://api.disneyapi.dev/character'); 
+    const response = await fetch ('https://stranger-things-api.fly.dev/api/v1/characters'); 
         //gaurd clause
     if(response.status != 200) {
         throw new Error(response);
     } else {
         //converts promise to json
     const data = await response.json();
-    console.log(data.data)
-    //this is unique to this API
-    // data.data.forEach((agent) => console.log(agent.name));
-    data.data.forEach((character) => document.querySelector("div.container").insertAdjacentHTML("beforeend", 
-        `<div class="card-body"> 
+    console.log(data)
+    data.forEach((character) => document.querySelector("div.container").insertAdjacentHTML("beforeend", 
+        `<div class="card-body "> 
             <h1 class="card-title">${character.name}</h1>
+            <h2 class="text-base">${character.gender}</h2>
+            <h2 class="text-base">${character.eyeColor}</h2>
         </div>`));
-};
-
+    }
+    //this is unique to this API
+    // data.data.forEach((agent) => console.log(agent.name))
     } catch (error) {
         alert ("hey i could not find that agent"
         )
     }
-}
-
-getData ();
+};
+ getData ();
