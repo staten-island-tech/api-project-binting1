5cc273909e79b7418f7bcfef68e9abcd1be46e10
binting1 <bintingchenn123@gmail.com>
2024-11-25 13:49:21 -0500

    123

--- app/main.js
+++ app/main.js
@@ -2,7 +2,7 @@ import "./style.css";
 async function getData () {
     try {
        //returns a promise
-    const response = await fetch ('https://stranger-things-api.fly.dev/api/v1/characters'); 
+    const response = await fetch ('https://stranger-things-api.fly.dev/api/v1/characters/?perPage=40'); 
         //gaurd clause
     if(response.status != 200) {
         throw new Error(response);
@@ -11,10 +11,11 @@ async function getData () {
     const data = await response.json();
     console.log(data)
     data.forEach((character) => document.querySelector("div.container").insertAdjacentHTML("beforeend", 
-        `<div class="card-body "> 
+        `<div class="card-body border-2 "> 
             <h1 class="card-title">${character.name}</h1>
             <h2 class="text-base">${character.gender}</h2>
-            <h2 class="text-base">${character.eyeColor}</h2>
+            <h2 class="text-base">${character.status}</h2>
+            <img src="${character.photo}"></img>
         </div>`));
     }
     //this is unique to this API
