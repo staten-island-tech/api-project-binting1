1c25fa370d39c7241d1e1e5e7cedb73f7a351f91
binting1 <bintingchenn123@gmail.com>
2024-12-02 14:02:18 -0500

    123

--- app/main.js
+++ app/main.js
@@ -2,7 +2,6 @@ import "./style.css";
 
 const DOMSelectors = { 
     container: document.querySelector(".container"), 
-    secContainer: document.querySelector(".apple"),
 }
 
 async function getData () {
@@ -25,21 +24,6 @@ async function getData () {
                     <img src="${character.photo}" alt="${character.name}" />
                 </div>`
             )); 
-
-            // function getDetails (){
-            //     document.querySelector(".btn").addEventListener("click", function () {
-            //         DOMSelectors.container.innerHTML = "";
-            //         data.forEach((character) => DOMSelectors.secContainer.insertAdjacentHTML("beforeend", 
-            //             `<div class="card-body border-2 rounded-xl"> 
-            //                 <h1 class="card-title">${character.name}</h1>
-            //                 <h2 class="text-base">${character.gender}</h2>
-            //                 <h2 class="text-base">${character.status}</h2>
-            //                 <img src="${character.photo}" alt="${character.name}" />
-            //             </div>`));
-            //     })
-            // }
-
-            // getDetails ();
         }
         } catch (error) {
         alert("Hey, I could not find that agent.");
@@ -48,33 +32,47 @@ async function getData () {
 
 getData();
 
+async function getFemale () {
+    document.querySelector(".btn").addEventListener("click", async function () {
+    DOMSelectors.container.innerHTML = "";
+    try { 
+        const response = await fetch("https://stranger-things-api.fly.dev/api/v1/characters/?perPage=40/&gender=Female")
 
-async function getDetails () {
-    try {
-        // Fetch data from the API
-        const response = await fetch('https://stranger-things-api.fly.dev/api/v1/characters/?perPage=40/?gender=Female'); 
+    if(response.status !== 200) {
+        throw new Error("failed to fetch data"); 
+    } else {
+        const data = await response.json();
+    console.log(data);
+    data.forEach((character) => DOMSelectors.container.insertAdjacentHTML("beforeend", 
+        `<div class="card card-body border-2 rounded-xl"> 
+            <h1 class="card-title">${character.name}</h1>
+            <h2 class="text-base">${character.gender}</h2>
+            <h2 class="text-base">${character.status}</h2>
+            <img src="${character.photo}" alt="${character.name}" />
+        </div>`
+    )); 
+    } 
+    } catch (error) {
+        alert("hey"); 
+    }
+    })
+}
+getFemale ();
 
-        // Guard clause for unsuccessful response
-        if(response.status !== 200) {
-            throw new Error("Failed to fetch data");
-        } else {
-            // Convert the response to JSON
-            const data = await response.json();
-            console.log(data);
-            DOMSelectors.container.innerHTML = "";
-            data.forEach((character) => DOMSelectors.secContainer.insertAdjacentHTML("beforeend", 
-                `<div class="card-body border-2 rounded-xl"> 
+function moreInfo () {
+    document.querySelector("div.card").addEventListener("click", function () {
+        DOMSelectors.container.innerHTML -"";
+        data.forEach((character) => DOMSelectors.container.insertAdjacentHTML("beforeend",
+            `<div class="card-body border-2 rounded-xl"> 
                     <h1 class="card-title">${character.name}</h1>
                     <h2 class="text-base">${character.gender}</h2>
                     <h2 class="text-base">${character.status}</h2>
                     <img src="${character.photo}" alt="${character.name}" />
                 </div>`
-            )); 
-        } 
-    }   catch (error) {
-        alert("Hey, I could not find that agent.");
-    }
-};
+        ));
+    })
+}; 
 
-getDetails ();
+moreInfo ();
+
 
