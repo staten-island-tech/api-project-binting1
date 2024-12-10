46c4e99690db446ed1770f387756d1e7db1b91b3
binting1 <bintingchenn123@gmail.com>
2024-12-10 14:01:29 -0500

    123

--- app/main.js
+++ app/main.js
@@ -16,7 +16,6 @@ async function getData () {
             // Convert the response to JSON
             const data = await response.json();
             console.log(data);
-            // const btn = DOMSelectors.container.querySelector(`.card-body:last-child .btn`);
             data.forEach((character) => DOMSelectors.container.insertAdjacentHTML("beforeend", 
                 `<div class="card-body border-2 rounded-xl"> 
                     <h1 class="card-title">${character.name}</h1>
@@ -26,31 +25,28 @@ async function getData () {
                     <button class="btn">more info</button>
                 </div>` 
             ));
-
-                btn.addEventListener("click", () => {
-                moreInfo(character); // Pass the character object to moreInfo()
-            });   
-        };
-        
+            };  
             
+            // function moreInfo () {
+            //     DOMSelectors.container.innerHTML = "";
+            //     data.forEach((character) => DOMSelectors.container.insertAdjacentHTML("beforeend",
+            //         `<div class="card-body border-2 rounded-xl"> 
+            //         <h1 class="card-title">${character.name}</h1>
+            //         <h2 class="text-base">${character.gender}</h2>
+            //         <h2 class="text-base">${character.status}</h2>
+            //         <img src="${character.photo}" alt="${character.name}" />
+            //     </div>`
+            //  ));
+            // };
+
+            // document.querySelector.btn.addEventListener. function moreInfo (); 
+
         } catch (error) {
         alert("Hey, I could not find that agent.");
     }
 };
 
 getData();
-
-function moreInfo () {
-    DOMSelectors.container.innerHTML = "";
-    forEach((character) => DOMSelectors.container.insertAdjacentHTML("beforeend",
-        `<div class="card-body border-2 rounded-xl"> 
-                <h1 class="card-title">${character.name}</h1>
-                <h2 class="text-base">${character.gender}</h2>
-                <h2 class="text-base">${character.status}</h2>
-                <img src="${character.photo}" alt="${character.name}" />
-            </div>`
-    ));
-}; 
 
 
 
